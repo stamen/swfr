2ac2519993ef4bef305168b00b7ee54e5e20fea4
Seth Fitzsimmons <seth@mojodna.net>
2015-03-06 22:21:34 -0800

    Concurrent workers
    Using async as a crutch for .times.  (It was either that or d3.range.)

--- activities.js
+++ activities.js
@@ -1,26 +1,43 @@
 "use strict";
+
+var os = require("os");
+
+var async = require("async");
+
 var swfr = require("./index");
 
-var worker = swfr({
-  domain: "SplitMerge",
-  taskList: "splitmerge_activity_tasklist"
-}, function(task, callback) {
-  console.log("worker #1:", task);
+var activityHandler = function(i) {
+  return function(task, callback) {
+    console.log("worker #%d:", i, task);
 
-  switch (task.activityType.name) {
-  case "SplitMergeActivity.noop":
-    if (Math.random() < 0.5) {
-      return callback(new Error("Synthetic error for input: " + task.input[0]));
+    switch (task.activityType.name) {
+    case "SplitMergeActivity.noop":
+      if (Math.random() < 0.5) {
+        return callback(new Error("Synthetic error for input: " + task.input[0]));
+      }
+
+      return callback(null, task.input[0]);
+    case "SplitMergeActivity.report_result":
+      return callback(null, task.input[0]);
+    default:
+      return callback(new Error("Unsupported activity type: " + task.activityType.name));
     }
+  };
+};
 
-    return callback(null, task.input[0]);
-  case "SplitMergeActivity.report_result":
-    return callback(null, task.input[0]);
-  default:
-    return callback(new Error("Unsupported activity type: " + task.activityType.name));
+async.times(os.cpus().length, function(i) {
+  return swfr({
+    domain: "SplitMerge",
+    taskList: "splitmerge_activity_tasklist"
+  }, activityHandler(i));
+}, function(err, workers) {
+  if (err) {
+    throw err;
   }
-});
 
-process.on("SIGTERM", function() {
-  worker.cancel();
+  process.on("SIGTERM", function() {
+    return workers.forEach(function(w) {
+      return w.cancel();
+    });
+  });
 });
