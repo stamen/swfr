3b63b4084ffbe7e70ca4d3d3ccda2876b2b6df2e
Seth Fitzsimmons <seth@mojodna.net>
2015-03-17 13:43:31 -0700

    Extract cell generation

--- cgiar-csi-srtm.js
+++ cgiar-csi-srtm.js
@@ -14,6 +14,57 @@ var CELL_PADDING = 1,
     // WGS 84 semi-major axis (m)
     SEMI_MAJOR_AXIS = 6378137;
 
+var tile = function(vrt, extent, targetZoom, targetPrefix) {
+  var cells = [],
+      widthPx = Math.pow(2, targetZoom + 8), // world's width in px, assuming 256x256 tiles
+      heightPx = widthPx,
+      // 2 * pi * earth radius * cos(lat)
+      circumference = 2 * Math.PI * SEMI_MAJOR_AXIS * Math.cos(0),
+      // extents
+      minX = (circumference / 2) * -1,
+      minY = minX,
+      maxX = (circumference / 2),
+      maxY = maxX,
+      // circumference / pixel width(zoom)
+      targetResolution = circumference / widthPx,
+      width = CELL_WIDTH * targetResolution,
+      height = width,
+      // human-readable extent components
+      left = extent[0][0],
+      right = extent[1][0],
+      bottom = extent[1][1],
+      top = extent[0][1];
+
+  // chop the (overlapping) world into cells
+  for (var yi = 0; yi < heightPx / CELL_HEIGHT; yi++) {
+    var y = heightPx / CELL_HEIGHT - yi - 1, // convert from TMS to XYZ coords (top-left origin)
+        y1 = Math.max(minY, (yi * height) - (circumference / 2) - (CELL_PADDING * targetResolution)),
+        y2 = Math.min(maxY, ((yi + 1) * height) - (circumference / 2) + (CELL_PADDING * targetResolution));
+
+    for (var xi = 0; xi < widthPx / CELL_WIDTH; xi++) {
+      var x1 = Math.max(minX, (xi * width) - (circumference / 2) - (CELL_PADDING * targetResolution)),
+          x2 = Math.min(maxX, ((xi + 1) * width) - (circumference / 2) + (CELL_PADDING * targetResolution));
+      // check intersection
+      if (((left <= x1 && x1 <= right) ||
+           (left <= x2 && x2 <= right)) &&
+          ((bottom <= y1 && y1 <= top) ||
+           (bottom <= y2 && y2 <= top))) {
+        // return a list and then run map in order to limit concurrency
+        cells.push({
+          source: vrt,
+          target: util.format("%sz%d/%d/%d.tiff", targetPrefix, targetZoom, xi, y),
+          options: {
+            targetExtent: [x1, y1, x2, y2],
+            targetResolution: [targetResolution, targetResolution]
+          }
+        });
+      }
+    }
+  }
+
+  return cells;
+};
+
 var worker = decider({
   sync: true,
   domain: "SplitMerge",
@@ -85,61 +136,8 @@ var worker = decider({
       // stash the extent for later use
       this.userData.extent = extent;
 
-      var cells = [],
-          widthPx = Math.pow(2, targetZoom + 8), // world's width in px, assuming 256x256 tiles
-          heightPx = widthPx,
-          // 2 * pi * earth radius * cos(lat)
-          circumference = 2 * Math.PI * SEMI_MAJOR_AXIS * Math.cos(0),
-          // extents
-          minX = (circumference / 2) * -1,
-          minY = minX,
-          maxX = (circumference / 2),
-          maxY = maxX,
-          // circumference / pixel width(zoom)
-          targetResolution = circumference / widthPx,
-          width = CELL_WIDTH * targetResolution,
-          height = width,
-          // human-readable extent components
-          left = extent[0][0],
-          right = extent[1][0],
-          bottom = extent[1][1],
-          top = extent[0][1];
-
-      // chop the (overlapping) world into cells
-      for (var yi = 0; yi < heightPx / CELL_HEIGHT; yi++) {
-        var y = heightPx / CELL_HEIGHT - yi - 1, // convert from TMS to XYZ coords (top-left origin)
-            y1 = Math.max(minY, (yi * height) - (circumference / 2) - (CELL_PADDING * targetResolution)),
-            y2 = Math.min(maxY, ((yi + 1) * height) - (circumference / 2) + (CELL_PADDING * targetResolution));
-
-        for (var xi = 0; xi < widthPx / CELL_WIDTH; xi++) {
-          var x1 = Math.max(minX, (xi * width) - (circumference / 2) - (CELL_PADDING * targetResolution)),
-              x2 = Math.min(maxX, ((xi + 1) * width) - (circumference / 2) + (CELL_PADDING * targetResolution));
-          // check intersection
-          if (((left <= x1 && x1 <= right) ||
-               (left <= x2 && x2 <= right)) &&
-              ((bottom <= y1 && y1 <= top) ||
-               (bottom <= y2 && y2 <= top))) {
-            // return a list and then run map in order to limit concurrency
-            cells.push({
-              source: "/vsicurl/http://s3.amazonaws.com/cgiar-csi-srtm.openterrain.org/cgiar-csi-srtm-4326.vrt",
-              target: util.format("s3://%s/%sz%d/%d/%d.tiff", bucket, webPrefix, targetZoom, xi, y),
-              options: {
-                targetExtent: [x1, y1, x2, y2],
-                targetResolution: [targetResolution, targetResolution]
-              }
-            });
-          }
-        }
-      }
-
-      this.log("Generating %d cells.", cells.length);
-
-      return cells;
+      return tile("/vsicurl/http://s3.amazonaws.com/cgiar-csi-srtm.openterrain.org/cgiar-csi-srtm-4326.vrt", extent, targetZoom, util.format("s3://%s/%s", bucket, webPrefix));
     })
-    // .filter(function(cell) { // TODO temporary
-    //   // only deal with a single cell for now
-    //   return cell.target === "s3://cgiar-csi-srtm.openterrain.org/3857/z12/10/338.tiff";
-    // })
     .map(function(cell) {
       return this.activity("resample", "1.0", cell.source, cell.target, cell.options);
     }, {
@@ -176,56 +174,7 @@ var worker = decider({
 
       targetZoom -= 2;
 
-      var cells = [],
-          widthPx = Math.pow(2, targetZoom + 8), // world's width in px, assuming 256x256 tiles
-          heightPx = widthPx,
-          // 2 * pi * earth radius * cos(lat)
-          circumference = 2 * Math.PI * SEMI_MAJOR_AXIS * Math.cos(0),
-          // extents
-          minX = (circumference / 2) * -1,
-          minY = minX,
-          maxX = (circumference / 2),
-          maxY = maxX,
-          // circumference / pixel width(zoom)
-          targetResolution = circumference / widthPx,
-          width = CELL_WIDTH * targetResolution,
-          height = width,
-          // human-readable extent components
-          left = extent[0][0],
-          right = extent[1][0],
-          bottom = extent[1][1],
-          top = extent[0][1];
-
-      // chop the (overlapping) world into cells
-      for (var yi = 0; yi < heightPx / CELL_HEIGHT; yi++) {
-        var y = heightPx / CELL_HEIGHT - yi - 1, // convert from TMS to XYZ coords (top-left origin)
-            y1 = Math.max(minY, (yi * height) - (circumference / 2) - (CELL_PADDING * targetResolution)),
-            y2 = Math.min(maxY, ((yi + 1) * height) - (circumference / 2) + (CELL_PADDING * targetResolution));
-
-        for (var xi = 0; xi < widthPx / CELL_WIDTH; xi++) {
-          var x1 = Math.max(minX, (xi * width) - (circumference / 2) - (CELL_PADDING * targetResolution)),
-              x2 = Math.min(maxX, ((xi + 1) * width) - (circumference / 2) + (CELL_PADDING * targetResolution));
-          // check intersection
-          if (((left <= x1 && x1 <= right) ||
-               (left <= x2 && x2 <= right)) &&
-              ((bottom <= y1 && y1 <= top) ||
-               (bottom <= y2 && y2 <= top))) {
-            // return a list and then run map in order to limit concurrency
-            cells.push({
-              source: vrt,
-              target: util.format("s3://%s/%sz%d/%d/%d.tiff", bucket, webPrefix, targetZoom, xi, y),
-              options: {
-                targetExtent: [x1, y1, x2, y2],
-                targetResolution: [targetResolution, targetResolution]
-              }
-            });
-          }
-        }
-      }
-
-      this.log("Generating %d cells for zoom %d.", cells.length, targetZoom);
-
-      return cells;
+      return tile(vrt, extent, targetZoom, util.format("s3://%s/%s", bucket, webPrefix));
     })
     .map(function(cell) {
       return this.activity("resample", "1.0", cell.source, cell.target, cell.options);
