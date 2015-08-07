1ecc1d6aee3f0a91cea88ff856c60382ffb60c21
Seth Fitzsimmons <seth@mojodna.net>
2015-08-06 19:03:45 -0700

    Update basic examples

--- examples/workflow.js
+++ examples/workflow.js
@@ -3,43 +3,40 @@
 var os = require("os"),
     util = require("util");
 
-var Promise = require("bluebird");
+var env = require("require-env");
 
-var decider = require("./decider");
+var decider = require("../decider");
+
+var ACTIVITY_NAME = "noop",
+    ACTIVITY_VERSION = "1.4";
 
 console.log("swfr@%s:%d", os.hostname(), process.pid);
 
 var worker = decider({
-  domain: "SplitMerge",
-  taskList: "splitmerge_workflow_tasklist"
-}, function(input) {
-  var context = this;
+  domain: env.require("AWS_SWF_DOMAIN")
+}, function(chain, input) {
+  return chain
+    .then(function() {
+      var values = [];
 
-  this.log("Running workflow");
-  this.log("Input:", input);
+      for (var x = 0; x < 10; x++) {
+      // for (var x = 0; x < 1100; x++) {
+        values.push(x);
+      }
 
-  var values = [];
+      return values;
+    })
+    .map(function(i) {
+      this.log("%s:%s:", ACTIVITY_NAME, ACTIVITY_VERSION, i);
+      this.status = util.format("%s@%s:", ACTIVITY_NAME, ACTIVITY_VERSION, i);
 
-  for (var x = 0; x < 1100; x++) {
-    values.push(x);
-  }
-
-  // TODO stick map in the context to manage concurrency?
-  // or just reiterate the fact that a workflow can have no more than 100
-  // activity tasks active at a time?
-  var calls = Promise.map(values, function(i) {
-    context.status = util.format("SplitMergeActivity.noop@1.4:", i);
-
-    return context.activity({
-      retries: 5
-      // TODO options (heartbeatTimeout, etc.)
-    })("SplitMergeActivity.noop", "1.4", i);
-  }, {
-    concurrency: 100
-  });
-
-  return Promise.all(calls)
-    .bind(context)
+      return this.activity({
+        retries: 5
+        // TODO options (heartbeatTimeout, etc.)
+      })(ACTIVITY_NAME, ACTIVITY_VERSION, i);
+    }, {
+      concurrency: 100 // TODO transparently manage concurrency?
+    })
     .then(function(result) {
       this.log("Output:", result);
 
@@ -55,18 +52,19 @@ var worker = decider({
       this.log("Sum:", sum);
 
       this.status = "print sum";
-      return this.activity({ retries: 5 })("SplitMergeActivity.noop", "1.4", sum);
+      return this.activity({ retries: 5 })(ACTIVITY_NAME, ACTIVITY_VERSION, sum);
     })
     .then(function(result) {
       this.log("Result:", result);
 
       this.status = "print done";
-      return this.activity({ retries: 5 })("SplitMergeActivity.noop", "1.4", "done");
+      return this.activity({ retries: 5 })(ACTIVITY_NAME, ACTIVITY_VERSION, "done");
     })
     .then(function() {
       this.log("done.");
 
       this.status = "done";
+
       this.complete("done");
     });
 });
